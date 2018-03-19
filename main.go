ac89e631b1a423704c91aa2bd14c34c5d2f59f82
lpuigo <laurent.puig@gmail.com>
2018-03-19 22:56:54 +0100

    add example for handleClose method

--- main.go
+++ main.go
@@ -16,14 +16,21 @@ func main() {
 type DemoElement struct {
 	*js.Object
 
+	Mark    string   `js:"mark"`
 	Visible bool     `js:"visible"`
 	VM      *hvue.VM `js:"vm"`
 }
 
 func NewDemoElement() *DemoElement {
 	de := &DemoElement{Object: js.Global.Get("Object").New()}
+	de.Mark = "troulala"
 	de.Visible = false
 	return de
+}
+
+func (de *DemoElement) HandleClose(done *js.Object) {
+	de.Mark = "itou"
+	done.Invoke()
 }
 
 func pressAButton() {
@@ -31,8 +38,12 @@ func pressAButton() {
 
 	hvue.NewVM(
 		hvue.El("#app"),
-		hvue.DataS(hvue.NewT(de)),
+		hvue.DataS(de),
 		hvue.MethodsOf(de),
+		hvue.Method("handleClose", func() {
+			de.Mark = "itourlilou"
+			de.Visible = false
+		}),
 	)
 
 	js.Global.Set("de", de)
